d66737025fcd8f4062728cfb888193a6d9d686a3
nishant31jan <87146449+nishant31jan@users.noreply.github.com>
2021-07-19 16:07:59 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,43 +1,65 @@
-var seaImg;
-var seablock;
-var shipImg1,ship2;
+var leaf1;
+var leafImg;
+var appleImg;
+var apple1;
+var garden,rabbit;
+var gardenImg,rabbitImg;
 
 function preload(){
-  
-  seaImg=loadImage("sea.png");
-  
-  shipImg1=loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
-
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  appleImg = loadImage("apple.png");
+  leafImg = loadImage("orangeLeaf.png");
 }
 
 function setup(){
-  createCanvas(400,400);
-
-  seablock=createSprite(400,200);
-  seablock.addImage("horizon",seaImg);
-  seablock.scale=0.2
   
- 
-  ship2=createSprite(200,280,50,30);
-ship2.addAnimation("rocking",shipImg1);
-ship2.scale=0.15
+  createCanvas(400,400);
+  
+// Moving background
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
 
-
-
+//creating boy running
+rabbit = createSprite(180,340,30,30);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
 }
 
 
-
 function draw() {
-  background("white");
+  background(0);
 
-  seablock.velocityX=-3
+  rabbit.x=mouseX;
+  
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
 
-  console.log(seablock.x)
+  createLeaves();
 
- if(seablock.x<0){
-   seablock.x=seablock.width/10
- }
+  createApples();
 
- drawSprites();
+  drawSprites();
+}
+
+function createApples()
+{
+  if(frameCount%80==0){
+  apple1=createSprite(200,0,20,20);
+  apple1.velocityY=3;
+  apple1.scale=0.07
+  apple1.x=Math.round(random(20,370));
+  apple1.addImage("apple",appleImg);
+}
+}
+
+function createLeaves()
+{
+  if(frameCount%80==0){
+  leaf1=createSprite(360,40,20,20);
+  leaf1.velocityY=3;
+  leaf1.scale=0.07
+  leaf1.x=Math.round(random(20,370));
+  leaf1.addImage("leaf",leafImg);
+}
 }
